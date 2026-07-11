bb588825462e421896e911ce67ea78a21cc2ced8
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-07-11 15:53:14 -0400

    Feature: Add AresTeleOpBuilder and TestPathAuto unit tests to expand ARES-FTC test coverage

--- TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/AresRobotTest.kt
+++ TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/AresRobotTest.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode
 import org.firstinspires.ftc.teamcode.config.TunerConstants
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class AresRobotTest {
@@ -22,5 +23,23 @@ class AresRobotTest {
         assertTrue("Max distance should be reasonable", TunerConstants.VISION_MAX_DISTANCE_METERS in 1.0..15.0)
         assertTrue("Max ambiguity should be reasonable", TunerConstants.VISION_MAX_AMBIGUITY in 0.0..1.0)
         assertTrue("Mahalanobis threshold should be reasonable", TunerConstants.VISION_MAHALANOBIS_THRESHOLD > 0.0)
+    }
+
+    @Test
+    fun testAresTeleOpBuilder() {
+        val builder = org.firstinspires.ftc.teamcode.dsl.AresTeleOpBuilder()
+        var initCalled = false
+        var loopCalled = false
+
+        builder.onInit { _, _ -> initCalled = true }
+        builder.onLoop { _, _, _ -> loopCalled = true }
+
+        assertNotNull("onInitBlock should be configured", builder.onInitBlock)
+        assertNotNull("onLoopBlock should be configured", builder.onLoopBlock)
+    }
+
+    @Test
+    fun testTestPathAutoMetadata() {
+        assertEquals("TestPath", TestPathAuto.pathName)
     }
 }
