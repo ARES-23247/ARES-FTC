8dcc728670cb6235f4fd43e90f4376c2aa318336
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-07-22 08:23:03 -0400

    refactor(ftc): clean up TestAuto boilerplate and centralize telemetry/indicator DSL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAuto.kt
@@ -6,39 +6,30 @@ import com.areslib.ftc.FtcMecanumRobot
 import com.areslib.state.Alliance
 import com.areslib.action.RobotAction
 
-abstract class TestAutoBase(private val alliance: Alliance) : FtcMecanumAutoBase<AresRobot>() {
-    override val pathName: String = "TestAuto"
-    
-    override fun buildRobot(): AresRobot {
-        /**
-         * Documentation for robot
-         */
-        val robot = AresRobot(hardwareMap, telemetry)
-        robot.base.store.dispatch(RobotAction.SetAlliance(alliance))
-        return robot
-    }
 
-    override fun getMecanumRobot(robot: AresRobot): FtcMecanumRobot {
-        return robot.base
-    }
-
-    override fun updateRobot(robot: AresRobot) {
-        robot.update()
-    }
-
-    override fun closeRobot(robot: AresRobot) {
-        robot.close()
-    }
-}
 /**
  * Documentation for TestAutoRed
  */
 
 @Autonomous(name = "TestAuto - RED", group = "ARES")
-class TestAutoRed : TestAutoBase(Alliance.RED)
+class TestAutoRed : org.firstinspires.ftc.teamcode.dsl.AresAutoBase() {
+    override val pathName: String = "TestAuto"
+    override fun buildRobot(): AresRobot {
+        val robot = super.buildRobot()
+        robot.base.store.dispatch(RobotAction.SetAlliance(Alliance.RED))
+        return robot
+    }
+}
 /**
  * Documentation for TestAutoBlue
  */
 
 @Autonomous(name = "TestAuto - BLUE", group = "ARES")
-class TestAutoBlue : TestAutoBase(Alliance.BLUE)
+class TestAutoBlue : org.firstinspires.ftc.teamcode.dsl.AresAutoBase() {
+    override val pathName: String = "TestAuto"
+    override fun buildRobot(): AresRobot {
+        val robot = super.buildRobot()
+        robot.base.store.dispatch(RobotAction.SetAlliance(Alliance.BLUE))
+        return robot
+    }
+}
